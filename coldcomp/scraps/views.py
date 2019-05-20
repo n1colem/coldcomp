bd18e2681d7efc701cc3fbb7a8dcac1f6a8e1bae
n1colem <nicolemeyer1@gmail.com>
2019-05-19 21:25:12 -0700

    add views and urls for scrap

--- coldcomp/scraps/views.py
+++ coldcomp/scraps/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from .models import Scrap
 
 # Create your views here.
+
+class ScrapList(ListView):
+
+    model = Scrap
+    context_object_name = 'scraps'
