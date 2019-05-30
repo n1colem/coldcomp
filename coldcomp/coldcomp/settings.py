88e3805ebf0bfd9ed604a8dc062269a85eeb062b
n1colem <nicolemeyer1@gmail.com>
2019-05-30 11:07:35 -0700

    partly addresses #19

--- coldcomp/coldcomp/settings.py
+++ coldcomp/coldcomp/settings.py
@@ -118,9 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/coldcomp/craps/static/scraps'
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIR=[os.path.join(BASE_DIR,"static"),
+STATICFILES_DIR=['/scraps/static/scraps/'
 ]
